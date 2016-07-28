1b3107227afa7f7ee8147a30ad9ca78fc38c1eb0
Subramani B R <subramanibr@subramanis-mac-mini.local>
2016-07-28 18:20:31 +0530

    InitialCommit

--- BackgroungMailSender/ViewController.m
+++ BackgroungMailSender/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#import "SKPSMTPMessage.h"
+#import "NSData+Base64Additions.h"
+#import "SVProgressHUD.h"
+@interface ViewController ()<SKPSMTPMessageDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *txtSenderMail;
+@property (weak, nonatomic) IBOutlet UITextField *txtSenderPassword;
+@property (weak, nonatomic) IBOutlet UITextField *txtReceiverMail;
 
 @end
 
@@ -18,10 +23,67 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+-(void) sendEmailInBackground {
+    [SVProgressHUD show];
+    [SVProgressHUD setDefaultStyle:SVProgressHUDStyleCustom];
+    [SVProgressHUD setBackgroundLayerColor:[UIColor clearColor]];
+    [SVProgressHUD setDefaultMaskType:SVProgressHUDMaskTypeBlack];
+    NSLog(@"Start Sending");
+    SKPSMTPMessage *emailMessage = [[SKPSMTPMessage alloc] init];
+    emailMessage.fromEmail = self.txtSenderMail.text; //sender email address
+    emailMessage.toEmail =self.txtReceiverMail.text;  //receiver email address
+    emailMessage.relayHost = @"smtp.gmail.com";
+    //emailMessage.ccEmail =@"your cc address";
+    //emailMessage.bccEmail =@"your bcc address";
+    emailMessage.requiresAuth = YES;
+    emailMessage.login = self.txtSenderMail.text; //sender email address
+    emailMessage.pass = self.txtSenderPassword.text; //sender email password
+    emailMessage.subject =@"This is for subject";
+    emailMessage.wantsSecure = YES;
+    emailMessage.delegate = self; // you must include <SKPSMTPMessageDelegate> to your class
+    NSString *messageBody = @"This is for message body";
+    //for example :   NSString *messageBody = [NSString stringWithFormat:@"Tour Name: %@\nName: %@\nEmail: %@\nContact No: %@\nAddress: %@\nNote: %@",selectedTour,nameField.text,emailField.text,foneField.text,addField.text,txtView.text];
+    // Now creating plain text email message
+    NSDictionary *plainMsg = [NSDictionary
+                              dictionaryWithObjectsAndKeys:@"text/plain",kSKPSMTPPartContentTypeKey,
+                              messageBody,kSKPSMTPPartMessageKey,@"8bit",kSKPSMTPPartContentTransferEncodingKey,nil];
+    emailMessage.parts = [NSArray arrayWithObjects:plainMsg,nil];
+    //in addition : Logic for attaching file with email message.
+    /*
+     NSString *filePath = [[NSBundle mainBundle] pathForResource:@"filename" ofType:@"JPG"];
+     NSData *fileData = [NSData dataWithContentsOfFile:filePath];
+     NSDictionary *fileMsg = [NSDictionary dictionaryWithObjectsAndKeys:@"text/directory;\r\n\tx-
+     unix-mode=0644;\r\n\tname=\"filename.JPG\"",kSKPSMTPPartContentTypeKey,@"attachment;\r\n\tfilename=\"filename.JPG\"",kSKPSMTPPartContentDispositionKey,[fileData encodeBase64ForData],kSKPSMTPPartMessageKey,@"base64",kSKPSMTPPartContentTransferEncodingKey,nil];
+     emailMessage.parts = [NSArray arrayWithObjects:plainMsg,fileMsg,nil]; //including plain msg and attached file msg
+     */
+    [emailMessage send];
+    // sending email- will take little time to send so its better to use indicator with message showing sending...
+}
+-(void)messageSent:(SKPSMTPMessage *)message{
+    [SVProgressHUD dismiss];
+    NSLog(@"delegate - message sent");
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Message sent." message:nil delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles: nil];
+    [alert show];
+}
+// On Failure
+-(void)messageFailed:(SKPSMTPMessage *)message error:(NSError *)error{
+    [SVProgressHUD dismiss];
+    // open an alert with just an OK button
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error!" message:[error localizedDescription] delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles: nil];
+    [alert show];
+    NSLog(@"delegate - error(%ld): %@", (long)[error code], [error localizedDescription]);
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)clickedSend:(id)sender {
+    if (self.txtSenderMail.text.length==0 || self.txtReceiverMail.text.length==0 || self.txtSenderPassword.text.length==0) {
+        [[[UIAlertView alloc]initWithTitle:@"Please fill the required fields" message:nil delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil]show];
+    }else{
+        [self sendEmailInBackground];
+    }
 }
 
 @end
