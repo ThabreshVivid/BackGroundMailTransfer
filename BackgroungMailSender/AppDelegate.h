3c3151c679f8767f74d2edff4df63d40c758d786
Subramani B R <subramanibr@subramanis-mac-mini.local>
2016-07-28 17:06:39 +0530

    Initial Commit

---
+++ BackgroungMailSender/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BackgroungMailSender
+//
+//  Created by Subramani B R on 7/28/16.
+//  Copyright © 2016 VividInfotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BackgroungMailSender/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BackgroungMailSender
+//
+//  Created by Subramani B R on 7/28/16.
+//  Copyright © 2016 VividInfotech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
